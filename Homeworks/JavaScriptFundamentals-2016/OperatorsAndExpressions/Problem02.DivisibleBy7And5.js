9cecff2bee2516293edff251c7e9acc15548dfd5
tdochev <tddochev@gmail.com>
2016-06-16 18:43:12 +0300

    Fixed file error

--- Homeworks/JavaScriptFundamentals-2016/OperatorsAndExpressions/Problem02.DivisibleBy7And5.js
+++ Homeworks/JavaScriptFundamentals-2016/OperatorsAndExpressions/Problem02.DivisibleBy7And5.js
@@ -1,11 +1,7 @@
 function solve(parmas) {
-	var i,
-		lenght,
-		divisibleBy7and5,
+	var divisibleBy7and5,
 		number = +parmas[0];
 
 	divisibleBy7and5 = !(number % 5) && !(number % 7);
 	console.log(divisibleBy7and5 + ' ' + number);
 }
-
-solve([140]);
